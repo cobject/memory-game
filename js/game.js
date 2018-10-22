5f1efe118efc4da50197ab601f6a8ec3d1b14625
cobject <cobject@gmail.com>
2018-10-23 00:05:53 +0900

    feature: Add interface for the game

---
+++ js/game.js
@@ -0,0 +1,50 @@
+const cards = [
+    [1, 1, 2, 2],
+    [3, 3, 4, 4],
+    [5, 5, 6, 6],
+    [7, 7, 8, 8]
+];
+
+function initialize() {
+    
+}
+
+function resetGame() {
+
+}
+
+function shuffle() {
+
+}
+
+function onFlip(event) {
+
+}
+
+function onTimer() {
+
+}
+
+function updateMoves() {
+
+}
+
+function isCardOpened() {
+    return false;
+}
+
+function matchCard() {
+    return false;
+}
+
+function isFinish() {
+    return false;
+}
+
+function showPopup() {
+
+}
+
+function rating() {
+
+}
